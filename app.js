71507c33bc478c8016ba6de65983df2898fd07f0
ganeshpaiya <36655930+ganeshpaiya@users.noreply.github.com>
2018-06-11 16:41:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -109,6 +109,10 @@ app.use(flash())
 app.use('/', index)
 app.use('/users', users)
 
-app.listen(3000, function(){
-	console.log('Server running at port 3000: http://127.0.0.1:3000')
-})
+var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var nodeEnv = process.env.NODE_ENV || 'unknown';
+
+app.listen(port, ip, function () {
+    console.log( "Listening on " + ip + ", Server Port " + port  );
+});
